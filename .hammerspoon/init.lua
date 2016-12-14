c538646cb844f59458f474da5e8c4248071c63ab
wangsongiam <wangsongiam@gmail.com>
2016-12-14 16:01:06 -0500

    some hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -31,6 +31,31 @@ end
 local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
 
+-- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+--   local win = hs.window.focusedWindow()
+--   local f = win:frame()
+--   local screen = win:screen()
+--   local max = screen:frame()
+--
+--   f.x = max.x
+--   f.y = max.y
+--   f.w = max.w / 2
+--   f.h = max.h
+--   win:setFrame(f)
+-- end)
+--
+-- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+--   local win = hs.window.focusedWindow()
+--   local f = win:frame()
+--   local screen = win:screen()
+--   local max = screen:frame()
+--
+--   f.x = max.x + (max.w / 2)
+--   f.y = max.y
+--   f.w = max.w / 2
+--   f.h = max.h
+--   win:setFrame(f)
+-- end)
 local modalKey = {"alt"}
 
 local resizeMappings = {
