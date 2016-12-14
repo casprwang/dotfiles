9fd56d99be6bb301a3d19d915f414551dba02151
wangsongiam <wangsongiam@gmail.com>
2016-12-14 15:41:13 -0500

    hammerspoon resizeMappings

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -11,3 +11,43 @@ hs.hotkey.bind({"ctrl"}, 'h', keyCode('left') ,  nil,   keyCode('left'))
 hs.hotkey.bind({"ctrl"}, 'j', keyCode('down') ,  nil,   keyCode('down') )
 hs.hotkey.bind({"ctrl"}, 'k', keyCode('up')   ,  nil,   keyCode('up') )
 hs.hotkey.bind({"ctrl"}, 'l', keyCode('right'),  nil,   keyCode('right') )
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  hs.alert.show("!!!!!!!!!!  Fuck the Fuck off  !!!!!!!!!!")
+end)
+
+
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
+
+local modalKey = {"alt"}
+
+local resizeMappings = {
+  h={x=0, y=0, w=0.5, h=1},
+  j={x=0, y=0.5, w=1, h=0.5},
+  k={x=0, y=0, w=1, h=0.5},
+  l={x=0.5, y=0, w=0.5, h=1},
+  m={x=0, y=0, w=1, h=1},
+  u={x=0, y=0, w=0.33, h=1},
+  i={x=0.33, y=0, w=0.33, h=1},
+  o={x=0.66, y=0, w=0.33, h=1},
+
+}
+
+for key in pairs(resizeMappings) do
+  hs.hotkey.bind(modalKey, key, function()
+    local win = hs.window.focusedWindow()
+    if win then win:moveToUnit(resizeMappings[key]) end
+  end)
+end
