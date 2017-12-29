cd9d30b03c7a5def2f39aa520923dc6465f2a7db
wangsongiam <wangsongiam@gmail.com>
2017-12-29 04:05:10 -0500

    finished lua scripts

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -299,96 +299,6 @@ appWatcher = hs.application.watcher.new(applicationWatcher)
 appWatcher:start()
 
 
--- try not to code
-
--- ref:  https://stackoverflow.com/questions/656199/search-for-an-item-in-a-lua-list
-function Set(list)
-  local set = {}
-  for _, l in ipairs(list) do set[l] = true end
-  return set
-end
-
-
-
-function postDeactivated(appName, startTime, endTime, duration)
-  local url1 = "https://first-touch-d70f5.firebaseio.com/periods.json"
-  local url2 = "https://first-touch-d70f5.firebaseio.com/editors.json"
-
-  local data1 = {
-    ["timestamp"] = { [".sv"] = "timestamp" },
-    ["editor"]= appName,
-    ["start"] = startTime,
-    ["end"] = endTime,
-    ["duration"] = duration
-  }
-  local json1 = hs.json.encode(data1)
-
-  local data2 = {
-    [appName] = {
-      ["timestamp"] = { [".sv"] = "timestamp" },
-      ["start"] = startTime,
-      ["end"] = endTime,
-      ["duration"] = duration
-    }
-  }
-  local json2 = hs.json.encode(data2)
-
-  hs.http.asyncPost(url1, json1, nil, function(status, data)
-    if(status ~= 200)then
-      hs.alert.show("post1 fucked up")
-    end
-  end)
-
-  hs.http.asyncPost(url2, json2, nil, function(status, data)
-    if(status ~= 200)then
-      hs.alert.show("post2 fucked up")
-    end
-  end)
-end
-
-function getJSDate()
-  bool, result = hs.javascript("new Date().toISOString()")
-  if(bool == false) then
-    hs.alert.show("Javascript fucked up")
-  end
-  return result
-end
-
-function formatFloat(float)
-  local str =  string.format( "%.5f", float)
-  local bool, result =  hs.javascript("parseFloat(".. str ..")")
-  return result
-end
-
-local startTime = getJSDate()
-local endTime = getJSDate()
-local unixTime = hs.timer.secondsSinceEpoch()
-
-
-function tryNotToCode(appName, eventType, appObject)
-  local editors = Set { "Code", "Atom", "MacVim", "iTerm2" }
-
-  if(eventType == hs.application.watcher.activated) then
-    if (editors[appName]) then
-      startTime = getJSDate()
-      unixTime = hs.timer.secondsSinceEpoch()
-    end
-  end
-
-  if (eventType == hs.application.watcher.deactivated) then
-    if (editors[appName]) then
-      endTime = getJSDate()
-
-      local duration = formatFloat(hs.timer.secondsSinceEpoch() - unixTime)
-      hs.alert.show(duration)
-
-      postDeactivated(appName, startTime, endTime, duration)
-    end
-  end
-end
-
-Watcher = hs.application.watcher.new(tryNotToCode)
-Watcher:start()
 
 -- iTerm2
 -- Code
@@ -397,5 +307,8 @@ Watcher:start()
 ------------------------------------------------------------
 hs.hotkey.bind({"cmd","ctrl", "shift"}, "w", function()
 
-  hs.alert.show(formatFloat(1.22))
+    hs.alert.show(formatFloat(1.22))
 end)
+
+
+
---
+++ .hammerspoon/try-not-to-code.lua
@@ -0,0 +1,126 @@
+-- try not to code
+
+-- ref:  https://stackoverflow.com/questions/656199/search-for-an-item-in-a-lua-list
+function Set(list)
+  local set = {}
+  for _, l in ipairs(list) do set[l] = true end
+  return set
+end
+
+
+
+function postLast(appName, startTime, endTime, duration)
+  local url1 = "https://first-touch-d70f5.firebaseio.com/periods.json"
+  local url2 = "https://first-touch-d70f5.firebaseio.com/editors/" .. appName ..".json"
+
+  local data1 = {
+    ["timestamp"] = { [".sv"] = "timestamp" },
+    ["editor"]= appName,
+    ["start"] = startTime,
+    ["end"] = endTime,
+    ["duration"] = duration
+  }
+  local json1 = hs.json.encode(data1)
+
+  local data2 = {
+    ["timestamp"] = { [".sv"] = "timestamp" },
+    ["start"] = startTime,
+    ["end"] = endTime,
+    ["duration"] = duration
+  }
+  local json2 = hs.json.encode(data2)
+
+  hs.http.asyncPost(url1, json1, nil, function(status, data)
+    if(status ~= 200)then
+      hs.alert.show("post1 fucked up")
+    end
+  end)
+
+  hs.http.asyncPost(url2, json2, nil, function(status, data)
+    if(status ~= 200)then
+      hs.alert.show("post2 fucked up")
+    end
+  end)
+end
+
+function getJSDate()
+  bool, result = hs.javascript("new Date().toISOString()")
+  if(bool == false) then
+    hs.alert.show("Javascript fucked up")
+  end
+  return result
+end
+
+function formatFloat(float)
+  local str =  string.format( "%.5f", float)
+  local bool, result =  hs.javascript("parseFloat(".. str ..")")
+  return result
+end
+
+
+
+local startTime = getJSDate()
+local endTime = getJSDate()
+local unixTime = hs.timer.secondsSinceEpoch()
+local lastApp = nil
+
+function tryNotToCode(appName, eventType, appObject)
+  local editors = Set { "Code", "Atom", "MacVim", "iTerm2" }
+
+  if(eventType == hs.application.watcher.activated) then
+
+    if(editors[appName]) then
+      -- last is not editor
+      if(editors[lastApp]) then
+        endTime = getJSDate()
+        local duration = formatFloat(hs.timer.secondsSinceEpoch() - unixTime)
+        postLast(lastApp, startTime, endTime, duration)
+      end
+      startTime = getJSDate()
+      unixTime = hs.timer.secondsSinceEpoch()
+    else
+      -- cur is not editor
+      if(editors[lastApp])then
+        endTime = getJSDate()
+        local duration = formatFloat(hs.timer.secondsSinceEpoch() - unixTime)
+        postLast(lastApp, startTime, endTime, duration)
+      end
+    end
+
+    lastApp = appName
+  end
+end
+
+
+    -- if(editors[curApp] and editors[appName]) then
+
+    --   local duration = formatFloat(hs.timer.secondsSinceEpoch() - unixTime)
+    --   hs.alert.show(duration)
+
+    --   postDeactivated(appName, startTime, endTime, duration)
+
+    --   startTime = getJSDate()
+    --   curApp = appName
+
+    -- elseif (editors[appName]) then
+    --   startTime = getJSDate()
+    --   unixTime = hs.timer.secondsSinceEpoch()
+    -- end
+  -- elseif (eventType == hs.application.watcher.deactivated) then
+    -- if (editors[appName]) then
+    --   endTime = getJSDate()
+
+    --   local duration = formatFloat(hs.timer.secondsSinceEpoch() - unixTime)
+    --   hs.alert.show(duration)
+
+    --   postDeactivated(appName, startTime, endTime, duration)
+    -- end
+
+Watcher = hs.application.watcher.new(tryNotToCode)
+Watcher:start()
+
+
+hs.hotkey.bind({"cmd","ctrl", "shift"}, "w", function()
+
+    hs.alert.show(formatFloat(1.22))
+end)
