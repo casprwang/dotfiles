2a3a02ca7c39a1a0730df8ec9235619d9c889eb9
wangsongiam <wangsongiam@gmail.com>
2018-09-10 23:27:58 -0700

    updated setup

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@
 
 set -e
 curl https://raw.githubusercontent.com/wangsongiam/dotfiles/master/.gitconfig > ~/.gitconfig
+
+sudo wget -O /usr/local/bin/rmate https://raw.github.com/aurora/rmate/master/rmate
+sudo chmod a+x /usr/local/bin/rmate
