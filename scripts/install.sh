d2535ca2c79d0b32478ec7b5907a682d5186c4c9
wangsongiam <wangsongiam@gmail.com>
2019-04-28 15:16:46 -0400

    simplified scripts

--- scripts/install.sh
+++
@@ -1,9 +0,0 @@
-echo 'You might need to change your default shell to zsh: `chsh -s /bin/zsh` (or `sudo vim /etc/passwd`)'
-
-dir="$HOME/Developer/Song"
-mkdir -p $dir
-cd $dir
-git clone --recursive https://github.com/wangsongiam/dotfiles.git
-
-ln -s ./vim/.vimrc ~/.vimrc
-ln -s ./.vim/ ~/.vim
