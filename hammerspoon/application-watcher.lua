8d3239e2d246a4859ee2c1e0616b0209e3763d0b
Song Wang <wangsongiam@gmail.com>
2020-04-19 23:07:32 -0700

    simplified editting

--- hammerspoon/application-watcher.lua
+++ hammerspoon/application-watcher.lua
@@ -3,17 +3,16 @@ local editting= require('editting')
 -- disable alt-v for Adobe's built-in shortcut
 local function applicationWatcher(appName, eventType, appObject)
   if (eventType == hs.application.watcher.activated) then
-    editting.enableEdittingKeys()
     if (string.find(appName, "iTerm") or (string.find(appName, "VIM")) or
       (string.find(appName, "Alacritty"))) then
       editting.disableEdittingKeys()
-    elseif not (string.find(appName, "iTerm") or
-      (string.find(appName, "VIM")) or (string.find(appName, "Alacritty"))) then
+    else
       editting.enableEdittingKeys()
     end
   end
 end
 
 
+editting.enableEdittingKeys()
 local appWatcher = hs.application.watcher.new(applicationWatcher)
 appWatcher:start()
