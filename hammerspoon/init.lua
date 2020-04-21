994d890f3f97f2a532342c1bb462e067afa61c39
Song Wang <wangsongiam@gmail.com>
2020-04-21 15:29:02 -0700

    refine window watcher

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,9 @@
 dofile('override-defaults.lua')
 dofile('key-mappings.lua')
 dofile('window-resize.lua')
+dofile('window-watcher.lua')
 
 require('application-watcher'):start()
 
 hs.alert.show("Config loaded")
+
--- hammerspoon/window-watcher.lua
+++ hammerspoon/window-watcher.lua
@@ -1,6 +1,7 @@
 local events = hs.uielement.watcher
+local utils = require('utils')
 
-watchers = {}
+local watchers = {}
 
 function init()
   appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
@@ -15,8 +16,45 @@ function init()
   end
 end
 
+
+function windowCnt(win)
+  return tablelength(watchers[win:application():pid()].windows)
+end
+
+function drawWindowCount(win)
+  local t = { window = win }
+  local textStyle = {
+    font = {
+      name = hs.styledtext.defaultFonts.boldSystem,
+      size = 72,
+    },
+    color = {white = 1, alpha = 1},
+  }
+  local rect = hs.geometry.rect(
+    100,
+    100,
+    100,
+    100
+  )
+  t.box = hs.drawing.rectangle(rect)
+  t.box:setLevel("overlay")
+  t.box:setFillColor({white = 0.125, alpha = 0.7})
+  t.box:setFill(true)
+  t.box:setStroke(false)
+  t.box:setRoundedRectRadii(8, 8)
+  t.box:setRoundedRectRadii(8, 8)
+  local textRect = hs.geometry.rect(100, 100, 100, 100)
+  local styledText = hs.styledtext.new(windowCnt(win), textStyle)
+  t.text = hs.drawing.text(textRect, styledText)
+  t.text:setLevel("overlay")
+  t.box:show()
+  t.text:show()
+  t.box:hide(1.5)
+  t.text:hide(1.5)
+end
+
 function handleGlobalAppEvent(name, event, app)
-  if     event == hs.application.watcher.launched then
+  if event == hs.application.watcher.launched then
     watchApp(app)
   elseif event == hs.application.watcher.terminated then
     -- Clean up
@@ -37,7 +75,7 @@ function watchApp(app, initializing)
   local watcher = app:newWatcher(handleAppEvent)
   watchers[app:pid()] = {watcher = watcher, windows = {}}
 
-  watcher:start({events.windowCreated, events.focusedWindowChanged})
+  watcher:start({events.windowCreated, events.focusedWindowChanged, events.mainWindowChanged})
 
   -- Watch any windows that already exist
   for i, window in pairs(app:allWindows()) do
@@ -45,13 +83,30 @@ function watchApp(app, initializing)
   end
 end
 
+
+function getWindowCount(appId)
+end
+
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+
+
 function handleAppEvent(element, event)
   if event == events.windowCreated then
     watchWindow(element)
-  elseif event == events.focusedWindowChanged then
+  elseif event == events.mainWindowChanged then
+    if element:isWindow() then
+      -- hs.alert.show(tablelength(watchers[element:application():pid()].windows))
+    end
+    -- print(element:id())
+    -- print(utils.print_table(watchers[1318]))
     -- Handle window change
   end
 end
+
 
 function watchWindow(win, initializing)
   local appWindows = watchers[win:application():pid()].windows
@@ -62,19 +117,23 @@ function watchWindow(win, initializing)
     watcher:start({events.elementDestroyed, events.windowResized, events.windowMoved})
 
     if not initializing then
-      hs.alert.show('window created: '..win:id()..' with title: '..win:title())
+      -- window created
+      drawWindowCount(win)
+      -- hs.alert.show('window created: '..win:id()..' with title: '..win:title())
+      -- hs.alert.show(tablelength(watchers[win:application():pid()].windows), .3)
     end
   end
 end
 
 function handleWindowEvent(win, event, watcher, info)
   if event == events.elementDestroyed then
+    drawWindowCount(win)
     watcher:stop()
     watchers[info.pid].windows[info.id] = nil
   else
     -- Handle other events...
   end
-  hs.alert.show('window event '..event..' on '..info.id)
+  -- hs.alert.show('window event '..event..' on '..info.id)
 end
 
 init()
