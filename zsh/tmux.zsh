4d3d722d6b5b016607f04ced824bac3c5e8605e3
Song Wang <wangsongiam@gmail.com>
2020-11-26 18:44:41 -0800

    use private session name

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -2,10 +2,10 @@ pouptmux() {
     width=${2:-80%}
     height=${2:-80%}
     session_name=$(tmux list-panes -t "$TMUX_PANE" -F '#S' | head -n1)
-    if [[ "$(tmux display-message -p -F "#{session_name}")" == p-* ]];then
+    if [[ "$(tmux display-message -p -F "#{session_name}")" == _* ]];then
         tmux detach-client
     else
-        p_session_name="p-${session_name}"
+        p_session_name="_${session_name}"
         tmux popup -d '#{pane_current_path}' -xC -yC -w$width -h$height -K -E -R "tmux attach -t ${p_session_name} || tmux new -s ${p_session_name}"
     fi
 }
