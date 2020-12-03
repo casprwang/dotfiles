5b13f9e22eb688c8870044316346eee505cf03e6
Song Wang <wangsongiam@gmail.com>
2020-12-03 10:31:42 -0800

    update obsidian keymap

--- .hammerspoon/obsidian.lua
+++ .hammerspoon/obsidian.lua
@@ -5,6 +5,19 @@ Obsidian.bindings.r = hs.hotkey.bind({"cmd"}, "return", function()
     hs.eventtap.keyStroke({"alt"}, "return", 0)
 end)
 
+
+Obsidian.bindings.left = hs.hotkey.bind({}, "left", function()
+    hs.eventtap.keyStroke({"cmd"}, '1', 0)
+    hs.eventtap.keyStroke({"cmd"}, '1', 0)
+end)
+
+
+Obsidian.bindings.right = hs.hotkey.bind({}, "right", function()
+    hs.eventtap.keyStroke({"cmd"}, '2', 0)
+    hs.eventtap.keyStroke({"cmd"}, '2', 0)
+end)
+
+
 function Obsidian:enable() for _, v in pairs(self.bindings) do v:enable() end end
 function Obsidian:disable() for _, v in pairs(self.bindings) do v:disable() end end
 
