279f0096e370804060e28dbb60bd57476c5124e6
Song Wang <wangsongiam@gmail.com>
2020-12-06 21:34:00 -0800

    add mouse button maps

--- .hammerspoon/base-keybindings.lua
+++ .hammerspoon/base-keybindings.lua
@@ -22,3 +22,29 @@ hs.hotkey.bind(KEYS.CS, "h",
 hs.hotkey.bind(KEYS.CS, "l",
                function() hs.eventtap.keyStroke({"shift"}, "right", 0) end)
 
+
+
+hs.hotkey.bind({'ctrl'}, ";",
+               function() 
+                    hs.eventtap.leftClick(hs.mouse.getAbsolutePosition(), 0)
+               end)
+
+
+-- Mouse side buttons
+local ev = hs.eventtap.event
+
+mouseButton = hs.eventtap.new(
+    {ev.types.otherMouseDown},
+    function(e)
+      local button = e:getProperty(
+        hs.eventtap.event.properties['mouseEventButtonNumber']
+      )
+      if button == 4 then
+          hs.eventtap.event.newKeyEvent("j", true):post()
+          hs.eventtap.event.newKeyEvent("j", false):post()
+      elseif button == 3 then
+        hs.eventtap.event.newKeyEvent("k", true):post()
+        hs.eventtap.event.newKeyEvent("k", false):post()
+      end
+    end
+):start()
